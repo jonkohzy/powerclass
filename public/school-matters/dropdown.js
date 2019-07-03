711824062ebec28c29008d783db51e9c8c6cb455
origami100 <origami10004@gmail.com>
2019-07-03 14:10:06 +0800

    dropdown

---
+++ public/school-matters/dropdown.js
@@ -0,0 +1,66 @@
+const drop  = () => {
+    const inputField = document.querySelector('.chosen-value');
+    inputField.placeholder = 'Type to filter';
+    const dropdown = document.querySelector('.value-list');
+    const dropdownArray = document.querySelectorAll('li');
+    dropdown.classList.add('open');
+    inputField.focus(); // Demo purposes only
+    let valueArray = [];
+    dropdownArray.forEach(item => {
+    valueArray.push(item.textContent);
+    });
+
+    const closeDropdown = () => {
+    dropdown.classList.remove('open');
+    }
+
+    inputField.addEventListener('input', () => {
+    dropdown.classList.add('open');
+    let inputValue = inputField.value.toLowerCase();
+    let valueSubstring;
+    if (inputValue.length > 0) {
+        for (let j = 0; j < valueArray.length; j++) {
+        if (!(inputValue.substring(0, inputValue.length) === valueArray[j].substring(0, inputValue.length).toLowerCase())) {
+            dropdownArray[j].classList.add('closed');
+        } else {
+            dropdownArray[j].classList.remove('closed');
+        }
+        }
+    } else {
+        for (let i = 0; i < dropdownArray.length; i++) {
+        dropdownArray[i].classList.remove('closed');
+        }
+    }
+    });
+
+    dropdownArray.forEach(item => {
+    item.addEventListener('click', (evt) => {
+        inputField.value = item.textContent;
+        dropdownArray.forEach(dropdown => {
+        dropdown.classList.add('closed');
+        });
+    });
+    });
+
+
+    inputField.addEventListener('focus', () => {
+    inputField.placeholder = 'Type to filter';
+    dropdown.classList.add('open');
+    dropdownArray.forEach(dropdown => {
+        dropdown.classList.remove('closed');
+    });
+    });
+
+    inputField.addEventListener('blur', () => {
+    inputField.placeholder = 'Select exam';
+    dropdown.classList.remove('open');
+    });
+
+    document.addEventListener('click', (evt) => {
+    const isDropdown = dropdown.contains(evt.target);
+    const isInput = inputField.contains(evt.target);
+    if (!isDropdown && !isInput) {
+        dropdown.classList.remove('open');
+    }
+    });
+}
